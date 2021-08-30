ebf6dd0d221cc50752b8b0e60f4c33cb66396947
Jeremias Lapalma <jerelapalma1@gmail.com>
2021-08-30 20:36:27 -0300

    "se mejoro el metodo sugerir ahora si funciona "

--- src/turismoEnLaTierraMediaGrupo4/Sistema.java
+++ src/turismoEnLaTierraMediaGrupo4/Sistema.java
@@ -4,53 +4,53 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Sistema {
- protected Atraccion [] atraccion;
+	protected Atraccion[] atraccion;
 	protected Usuario[] usuarios;
+	protected PromocionBase[] promocion;
 
 	public Sistema(int numAtracciones, int numPromociones, int numUsuarios) {
-     this.atraccion = new Atraccion[numAtracciones];
+		this.atraccion = new Atraccion[numAtracciones];
 		this.usuarios = new Usuario[numUsuarios];
+		this.promocion = new PromocionBase[numPromociones];
 	}
 
 	/*
-	 * como las sugericias son para el usuario y usuario entiende promocionBase y
-	 * atraccion no hace falta dos arrays nuevos
-	 * 
-	 * @param usuario se pasa un usuario al cual se le va a sugerir la atraccion y
-	 * la promocion si acepta alguna promocion se guardara su sugerencia en un nuevo
-	 * array de usuarios que es lo que espera el metodo
+	 * @Param  se pasa por parametro una lista de usuarios 
+	 *  @Variable  cant  sirve para cuando se cumple la condicion valla contrando todas las que cumplen
+	 *  @variable indice  es para que pueda recorrer el segundo arrayy donde se van a guardar las sugerencias 
+	 *  ahora si funciona xdxd
 	 */
-	public Usuario[] sugerir(Usuario usuario) {
-		Usuario[] lista = new Usuario[usuarios.length];
-
-		
-		  
-		for (Usuario usuario1 : lista) {	
-			for (int i = 0; i < lista.length; i++) {
-				try {
-					while(!atraccion[i].llena()) {
-					if ((usuario1.getTipoFavorito().equals(usuario.getPromociones()[i].getTipo()))
-							&& (usuario1.getPresupuesto()) >= usuario.getPromociones()[i].getCosto()
-							&& usuario1.getTiempoDisponible() >= usuario.getPromociones()[i].getTiempo()) {
-
-						lista[i] = usuario1;
-
-					}}
-				} catch (ArrayIndexOutOfBoundsException a) {
-					System.out.println("la posicion a la cual esta queriendo acceder esta fuera del limite del array");
+	public Usuario[] sugerir(Usuario usuarioList[]) {
+		int cant = 0;
+		int indice = 0;
+		for (Usuario usuario1 : usuarioList) {
+			for (PromocionBase pr : promocion) {
+				while(!pr.llena()) {
+				if ((usuario1.getTipoFavorito().equals(pr.getTipo())) && (usuario1.getPresupuesto()) >= pr.getCosto()
+						&& usuario1.getTiempoDisponible() >= pr.getTiempo()) {
+					cant++;
 				}
-			}}
-		
-		return lista;
-}
-	
+			}}}	
+		this.usuarios =new Usuario[cant];
+		for (Usuario usuario2 : usuarioList) {
+		for(PromocionBase pr2 : promocion) {		
+			if ((usuario2.getTipoFavorito().equals(pr2.getTipo())) && (usuario2.getPresupuesto()) >= pr2.getCosto()
+					&& usuario2.getTiempoDisponible() >= pr2.getTiempo()) {
+				usuarios[indice] = usuario2;
+				indice++;
+			}	
+		}}
+
+		return usuarios;
+	}
+
 	/*
-	 * @Param c  se pasa por parametro el criterio por el cual se va a ordenar
+	 * @Param c se pasa por parametro el criterio por el cual se va a ordenar
 	 */
 	public void ordenar(Comparator<Atraccion> c) {
-		Arrays.sort(atraccion, c);;
+		Arrays.sort(atraccion, c);
+		;
 	}
-	
 
 	// errores que veo tanto en esta como la de joa es que al iinvocar getCosto() no
 	// se sabe a que promocion esta dirigiendose
