aa14a27c6c65cefacc12d6d7fd36c87a469f2930
JoanaBugarin <wooli3000@gmail.com>
2021-09-13 18:09:42 -0300

    agregada documentacion a clase EscribirItinerarios

--- src/turismoEnLaTierraMediaGrupo4/EscribirItinerarios.java
+++ src/turismoEnLaTierraMediaGrupo4/EscribirItinerarios.java
@@ -6,6 +6,13 @@ import java.io.PrintWriter;
 
 public class EscribirItinerarios {
 	
+	/*
+	 * @param usuario pasa un usuario para que la salida de itinerarios se pueda
+	 * realizar para todos los usuarios del sistema
+	 * 
+	 * salidaItinerario escribe y guarda en un archivo distinto para cada usuario
+	 * su itinerario al finalizar las sugerencias.
+	 */
 	public static void salidaItinerario(Usuario usuario) throws IOException {
 		String nombreArchivo = usuario.getNombre();
 		PrintWriter salida = new PrintWriter(new FileWriter(nombreArchivo+".out"));
