15d765b6fe064a313d061ee3e3d65744aa4f189a
PatriciaMariela <pachuberna@gmail.com>
2021-10-20 16:51:32 -0300

    Esqueleto itinerarioDAOimpl

---
+++ src/dao/ItinerarioDAOImpl.java
@@ -0,0 +1,37 @@
+package dao;
+
+import java.util.List;
+
+import turismoEnLaTierraMediaGrupo4.ManejadorArchivos;
+import turismoEnLaTierraMediaGrupo4.Usuario;
+
+public class ItinerarioDAOImpl implements ItinerarioDAO {
+
+	@Override
+	public List<ManejadorArchivos> findAll() {
+		return null;
+	}
+
+	@Override
+	public int countAll() {
+		return 0;
+	}
+
+	@Override
+	public int insert(ManejadorArchivos t) {
+		return 0;
+	}
+
+	@Override
+	public int update(ManejadorArchivos t) {
+		return 0;
+	}
+
+	@Override
+	public int delete(ManejadorArchivos t) {
+		return 0;
+	}
+
+
+
+}
