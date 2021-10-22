8234abed9d91cc5d5f2f1eca522e566cdc0e5b41
PatriciaMariela <pachuberna@gmail.com>
2021-10-22 17:11:50 -0300

    Se completan metodos insert, update, delete,findByID_Usuario() y toIty

--- src/dao/ItinerarioDAOImpl.java
+++ src/dao/ItinerarioDAOImpl.java
@@ -1,18 +1,82 @@
 package dao;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import turismoEnLaTierraMediaGrupo4.ManejadorArchivos;
-import turismoEnLaTierraMediaGrupo4.Usuario;
+import jdbc.ConnectionProvider;
+import turismoEnLaTierraMediaGrupo4.EscribirItinerarios;
+
+
 
 public class ItinerarioDAOImpl implements ItinerarioDAO {
 
 	private AtraccionDAOImpl atraccionDAO;
 	private PromocionDAOImpl promocionDAO;
 	private UsuarioDaoImpl usuarioDAO;
-	
+
 	@Override
-	public List<ManejadorArchivos> findAll() {
+	public int insert(EscribirItinerarios itinerario)  {
+		try {
+			String sql = "INSERT INTO Itinerario (ID_Promocion, ID_Itinerario, ID_Usuario, ID_Atraccion) VALUES (?,?,?,?)";
+			Connection conn;
+			conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+
+			statement.setInt(1, 0);
+			statement.setInt(2, 0);
+			statement.setInt(3, 0);
+			statement.setInt(4, 0);
+
+			return 0;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+		
+	}
+
+	@Override
+	public int update(EscribirItinerarios itinerario) throws SQLException {
+		try {
+			String sql = "UPDATE ITINERARIO SET ID = ? WHERE ID = ?";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, 0);
+			statement.setInt(2, 0);
+			statement.setInt(3, 0);
+			statement.setInt(4, 0);
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+	
+
+	@Override
+	public int delete(EscribirItinerarios itinerario) {
+		try {
+			String sql = "DELETE FROM ITINERARIO WHERE ID = ?";
+			Connection conn = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, 0);
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+	}
+
+
+	@Override
+	public List<EscribirItinerarios> findAll() {
 		return null;
 	}
 
@@ -22,20 +86,28 @@ public class ItinerarioDAOImpl implements ItinerarioDAO {
 	}
 
 	@Override
-	public int insert(ManejadorArchivos t) {
-		return 0;
+	public EscribirItinerarios findByID_Usuario() {
+		try {
+			String sql = "SELECT * FROM ITINERARIO WHERE ID = ?";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, 0);
+			ResultSet resultados = statement.executeQuery();
+
+			EscribirItinerarios itinerario = null;
+
+			if (resultados.next()) {
+				itinerario = toIty(resultados);
+			}
+
+			return itinerario;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
-	@Override
-	public int update(ManejadorArchivos t) {
-		return 0;
+	private EscribirItinerarios toIty(ResultSet resultados) throws IOException {
+		return (EscribirItinerarios) (resultados);
 	}
-
-	@Override
-	public int delete(ManejadorArchivos t) {
-		return 0;
-	}
-
-
 
 }
